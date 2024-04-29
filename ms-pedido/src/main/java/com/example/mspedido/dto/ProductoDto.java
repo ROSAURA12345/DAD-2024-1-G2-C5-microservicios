ee573ff4ef11b2aa4202ae9e316ced12e4343b47
Rosaura <rosaurayanapari123@gmail.com>
2024-04-29 10:16:49 -0500

    completo

---
+++ ms-pedido/src/main/java/com/example/mspedido/dto/ProductoDto.java
@@ -0,0 +1,16 @@
+package com.example.mspedido.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Data
+public class ProductoDto {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private LocalDate fecha_creacion;
+    private LocalDate fecha_fin;
+    private CategoriaDto categoriaDto;
+}
